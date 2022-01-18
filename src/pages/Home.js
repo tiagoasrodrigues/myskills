ca9856455922d939667cfc022ef83149cc677a93
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2022-01-18 14:42:06 -0300

    :tada: Iniciando projeto

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text } from 'react-native'
+
+export function Home() {
+  return (
+
+    <View
+      style={{
+        flex:
+          1,
+        justifyContent:
+          'center',
+        alignItems:
+          'center',
+      }}
+    >
+      <Text>Tiago Augusto</Text>
+      <View>
+        <Text>Desenvolvedor Front-End and Mobile</Text>
+      </View>
+    </View>
+  )
+}
