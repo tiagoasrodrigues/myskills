cd2c88e7abcd06610ed5a3346199978bd366918f
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2022-01-20 12:40:54 -0300

    :hammer: Refatorando componente SkillCard

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import {
   TouchableOpacity,
   Text,
-  StyleSheet
+  StyleSheet,
+  TouchableOpacityProps
 } from 'react-native';
 
-export function SkillCard({ skill }) {
+interface SkillCardProps extends TouchableOpacityProps {
+  skill: string;
+}
+
+export function SkillCard({ skill, ...rest }: SkillCardProps) {
   return (
-    <TouchableOpacity style={styles.buttonSkill}>
+    <TouchableOpacity
+      style={styles.buttonSkill}
+      {...rest}
+    >
       <Text style={styles.textSkill}>
         {skill}
       </Text>
